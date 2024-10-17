9e5ba79f4930bf95ff7f0dcb95ddddfd283e3628
Kenneth David Leonel Triana <60140555+kennethLeonel@users.noreply.github.com>
2024-10-16 19:26:39 -0500

    se hizo cambios

---
+++ src/libreriasUso.py
@@ -0,0 +1,15 @@
+# importar librerias
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import datetime
+from scipy import stats
+import holidays
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
+
+# Configuración warnings
+# ==============================================================================
+import warnings
+warnings.filterwarnings('ignore')
